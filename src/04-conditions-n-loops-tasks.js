4e645a4c90d04d009634995db53e60e3ec1ae397
OlgaPyryeva <vashkevicholga@gmail.com>
2023-09-09 13:33:11 +0300

    feat: add some loop task

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -292,8 +292,17 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = 0;
+  const arrNumber = String(num).split('').map(Number);
+
+  arrNumber.forEach((number) => {
+    sum += number;
+    if (sum > 9) {
+      sum -= 9;
+    }
+  });
+  return sum;
 }
 
 /**
