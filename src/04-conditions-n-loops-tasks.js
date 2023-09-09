8d36212aae8e912cde3aa65663eedafc2d046d81
OlgaPyryeva <vashkevicholga@gmail.com>
2023-09-09 13:19:35 +0300

    feat: add some loop tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -213,8 +213,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -229,8 +229,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return String(num).split('').reverse().join('');
 }
 
 /**
@@ -253,8 +253,30 @@ function reverseInteger(/* num */) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+
+function isCreditCardNumber(ccn) {
+  const str = String(ccn);
+  let checksum = 0;
+  const lastNumber = str.charAt(str.length - 1);
+  const cardnumber = String(ccn).split('').slice(0, -1).map(Number);
+
+  cardnumber.forEach((number, index) => {
+    if (index % 2 !== 0) {
+      const n = number * 2;
+
+      if (n > 9) {
+        checksum += n - 9;
+      } else {
+        checksum += n;
+      }
+    } else {
+      checksum += number;
+    }
+  });
+  const result = checksum % 10;
+  console.log(result, 'result');
+  console.log(lastNumber);
+  return 10 - result === +lastNumber;
 }
 
 /**
